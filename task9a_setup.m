f20615f9171f39659cbbf047b12609bd397fea18
LaithKu <119522324+LaithKu@users.noreply.github.com>
2025-03-07 16:37:42 +0100

    Task9 Zusatz

--- task9a_setup.m
+++ task9a_setup.m
@@ -1,16 +1,23 @@
 %%
-robot = importrobot('abbIrb1600.urdf');
-robotRBT = loadrobot("abbIrb120");
+%robot = importrobot('abbIrb1600.urdf');
+robotRBT = loadrobot("abbIrb1600"); % abbIrb1600/abbIrb120
 homeConfig = robotRBT.homeConfiguration;
 ik = inverseKinematics("RigidBodyTree",robotRBT);
 eeName = 'tool0'; 
 ikInitGuess = homeConfig; 
-numberOfSamples = 51;
+numberOfSamples = 100;%51;
 %%
-q = squeeze(out.q);
-qd = squeeze(out.qd);
-qdd = squeeze(out.qdd);
+
+
+q_lang = squeeze(out.q)';
+qd_lang = squeeze(out.qd)';
+qdd_lang = squeeze(out.qdd)';
 tvec = out.tvec;
+
+step = floor(size(q_lang, 2) / numberOfSamples);
+q = q_lang(:, 1:step:end);
+qd = qd_lang(:, 1:step:end);
+qdd = qdd_lang(:, 1:step:end);
 
 % Set up plot
 show(robotRBT,'Frames','off','PreservePlot',true); 
@@ -35,21 +42,21 @@ hold off;
 
 figure;
 subplot(3, 1, 1);
-plot(tvec, q);
+plot(tvec, q_lang);
 xlabel('Time (s)');
 ylabel('Joint Angles (rad)');
 legend('Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6');
 grid on;
 
 subplot(3, 1, 2);
-plot(tvec, qd);
+plot(tvec, qd_lang);
 xlabel('Time (s)');
 ylabel('Joint Velocities (rad/s)');
 legend('Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6');
 grid on;
 
 subplot(3, 1, 3);
-plot(tvec, qdd);
+plot(tvec, qdd_lang);
 xlabel('Time (s)');
 ylabel('Joint Accelerations (rad/s^2)');
 legend('Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6');
